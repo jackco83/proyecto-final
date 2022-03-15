ea35cb16fb704d8a461221a2063a8a6e1d1ae668
Jack Cruzado Ocaris <jackco83@hotmail.com>
2022-03-15 11:45:38 -0500

    Delete src directory

--- src/components/CartWidget.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-const CartWidget = () => {
-  return (
-    <Link to="/" className="shop"><i className="fa-solid fa-cart-shopping"></i></Link>
-  )
-}
-
-export default CartWidget;
